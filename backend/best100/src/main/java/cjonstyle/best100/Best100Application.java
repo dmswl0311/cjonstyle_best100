6a5998cb4a0e9657e8628251051e8e59248e90d2
dmswl0311 <dmswl_0311@naver.com>
2022-06-08 17:52:06 +0900

    init: spring boot 프로젝트 생성

---
+++ backend/best100/src/main/java/cjonstyle/best100/Best100Application.java
@@ -0,0 +1,13 @@
+package cjonstyle.best100;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Best100Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Best100Application.class, args);
+	}
+
+}
---
+++ backend/best100/src/test/java/cjonstyle/best100/Best100ApplicationTests.java
@@ -0,0 +1,13 @@
+package cjonstyle.best100;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Best100ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
