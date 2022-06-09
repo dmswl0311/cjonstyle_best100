0f7fc24cd11b01e10ab76c24d735d48d97627994
dmswl0311 <dmswl_0311@naver.com>
2022-06-09 13:36:47 +0900

    fix: api 경로 수정

--- backend/best100/src/main/java/cjonstyle/best100/controller/OpinionController.java
+++ backend/best100/src/main/java/cjonstyle/best100/controller/OpinionController.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/opinion")
 @RequiredArgsConstructor
 @Slf4j
 public class OpinionController {
